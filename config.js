6ca993c10d8fdf260273841677cf5a79ef84c18a
hannes <hannes.mitterer@gmail.com>
2025-12-21 19:52:59 -0400

    Add configuration file for sentinel settings

---
+++ config.js
@@ -0,0 +1,17 @@
+{
+  "SENTINEL_CONFIG": {
+    "VERSION": "2.2",
+    "HVAR_THRESHOLD": 0.043,
+    "TREASURY_EOA": "0x5d614B...944b",
+    "ENDPOINTS": {
+      "TELEGRAM_BOT": "YOUR_TELEGRAM_TOKEN",
+      "DISCORD_WEBHOOK": "YOUR_DISCORD_URL",
+      "API_AIC": "https://api.hannesmitterer.io/v1"
+    },
+    "NOTIFICATIONS": {
+      "PUSH_ENABLED": true,
+      "GHOST_MODE": true,
+      "ANONYMITY_LEVEL": "MAX"
+    }
+  }
+}
